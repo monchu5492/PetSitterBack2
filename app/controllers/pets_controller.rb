31e79f4d6678924a4369cb12786e490dabac23b4
Alice Won <alicewon4@gmail.com>
2020-02-13 12:09:24 -0800

    thursday backend updates to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,19 +3,16 @@ require 'byebug'
 class PetsController < ApplicationController
   def index
     pets = Pet.all
+    puts pets
     render json: pets
   end
 
   def show
-      pet = Pet.where(pet_id: params[:id])
+      pet = Pet.find(params[:id])
       render json: pet
   end
 
   def create
-<<<<<<< HEAD
-    byebug
-=======
->>>>>>> e3e29ec2d2062b6d66ad0499139c02d2eb9ffb95
       pet = Pet.new(params.require(:pet).permit(:name, :image, :anmial_type, :age, :owner_id))
       if pet.save
           render json: pet   
@@ -30,9 +27,17 @@ class PetsController < ApplicationController
       render json: pet
   end
 
+#   def destroy
+#       pet = Pet.find(params[:id])
+#       pet.destroy
+#       render json: {message: 'Your pet has been removed!'}
+#   end
+
   def destroy
-      pet = Pet.find(params[:id])
-      pet.destroy
-      render json: {message: 'Your pet has been removed!'}
+    @pet = Pet.find(params[:id])
+    @temp = @pet
+    @pet.delete
+    render :json => @temp 
+    # {message: 'Your pet has been removed!'}
   end
 end
--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,4 +1,4 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :anmial_type, :age, notes:
+  attributes :id, :name, :image, :anmial_type, :age, :notes
   has_one :owner
 end
